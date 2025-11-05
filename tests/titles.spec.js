491e2a58b44c1fd091fc0b3c295be771a50e5467
Grace Melody <gracemelodykristanti@gmail.com>
2025-11-05 14:32:02 +0900

    Added titles scenario: Happy flow multiple page multiple result

--- tests/titles.spec.js
+++ tests/titles.spec.js
@@ -21,3 +21,23 @@ test("Happy flow - One page one result", async ({ page }) => {
     tmdbMainPage.page.locator("p[class='text-blue-500 font-bold py-1']")
   ).toHaveCount(1);
 });
+
+test("Happy flow - Multiple page multiple result", async ({ page }) => {
+  const tmdbMainPage = new MainPage(page);
+  const API_URL_PATTERN = "https://api.themoviedb.org/3/search/movie?query";
+  await tmdbMainPage.gotoMainPage();
+  await tmdbMainPage.searchInBar("Ghibli");
+
+  const [response] = await Promise.all([
+    tmdbMainPage.page.waitForResponse(
+      (response) =>
+        response.url().includes(API_URL_PATTERN) && response.status() === 200
+    ),
+  ]);
+  // Expect two pages of result
+  expect(tmdbMainPage.page.locator("a[aria-label^='Page']")).toHaveCount(2);
+  // Expect the first result to have word "Ghibli" in it
+  expect(
+    tmdbMainPage.page.locator("p[class='text-blue-500 font-bold py-1']").first()
+  ).toContainText("Ghibli");
+});
